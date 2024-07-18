482157182556f655f408d089bbb6f91b7e4ffa09
WaldenBingham <sebastian@amler.de>
2024-07-18 23:21:34 +0200

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,48 +5,85 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-}
-
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records - DONE
-      // Filter the users array for any user with the same username and password
-    let validusers = users.filter((user) => {
-        return (user.username === username && user.password === password);
-    });
-    // Return true if any valid user is found, otherwise false
-    if (validusers.length > 0) {
+const isValid = (user)=>{ //returns boolean
+    let filtered_users = users.filter((user)=> user.username === user);
+    if(filtered_users){
         return true;
-    } else {
-        return false;
     }
+    return false;
+}
+const authenticatedUser = (username,password)=>{ //returns boolean
+    if(isValid(username)){
+        let filtered_users = users.filter((user)=> (user.username===username)&&(user.password===password));
+        if(filtered_users){
+            return true;
+        }
+        return false;
+       
+    }
+    return false;
+    
+
 }
 
-//only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here - DONE
-      const username = req.body.username;
+regd_users.post("/register", (req,res) => {
+    //Write your code here - DONE DONE
+    const username = req.body.username;
     const password = req.body.password;
-    // Check if both username and password are provided
-    if (username && password) {
-        // Check if the user does not already exist
-        if (!doesExist(username)) {
-            // Add the new user to the users array
-            users.push({"username": username, "password": password});
-            return res.status(200).json({message: "User successfully registered. Now you can login"});
-        } else {
-            return res.status(404).json({message: "User already exists!"});
+    if(username&&password){
+        const present = users.filter((user)=> user.username === username)
+        if(present.length===0){
+            users.push({"username":req.body.username,"password":req.body.password});
+            return res.status(201).json({message:"USer Created successfully"})
+        }
+        else{
+          return res.status(400).json({message:"Already exists"})
         }
     }
-    // Return error if username or password is missing
-    return res.status(404).json({message: "Unable to register user."});
+    else if(!username && !password){
+      return res.status(400).json({message:"Bad request"})
+    }
+    else if(!username || !password){
+      return res.status(400).json({message:"Check username and password"})
+    }
+  
+   
+  });
+
+//only registered users can login - DONE DONE
+regd_users.post("/login", (req,res) => {
+    let user = req.body.username;
+    let pass = req.body.password;
+    if(!authenticatedUser(user,pass)){
+        return res.status(403).json({message:"User not authenticated"})
+    }
+
+    let accessToken = jwt.sign({
+        data: user
+    },'access',{expiresIn:60*60})
+    req.session.authorization = {
+        accessToken
+    }
+    res.send("User logged in Successfully")
+ 
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //Write your code here - DONE DONE
+  let userd = req.session.username;
+  let ISBN = req.params.isbn;
+  let details = req.query.review;
+  let rev = {user:userd,review:details}
+  books[ISBN].reviews = rev;
+  return res.status(201).json({message:"Review added successfully"})
+  
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    let ISBN = req.params.isbn;
+    books[ISBN].reviews = {}
+    return res.status(200).json({messsage:"Review has been deleted"})
 });
 
 module.exports.authenticated = regd_users;
